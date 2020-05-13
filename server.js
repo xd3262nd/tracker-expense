eb0b69085ca485a6634f1f490f2902cff2f33eb5
qian587 <tanxx587@umn.edu>
2020-05-12 21:33:27 -0500

    edit server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 let api_routes = require('./routes/api.js')
-let moment = require('moment')
+let path = require('path')
 
 // App config
 let app = express()
@@ -9,6 +9,9 @@ let app = express()
 app.use(bodyParser.json())
 
 app.use('/api', api_routes)
+
+//Serves the Vue aoo
+app.use(express.static(path.join(__dirname, 'client', 'dist')))
 
 // Start server running
 let server = app.listen(process.env.PORT || 3000, function() {
